343c9ecd5bdf312914d9fd8c13db546ca5c548f4
db360 <da.b@hotmail.es>
2021-12-08 21:36:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/** Connect to Moralis server */
+const serverUrl = "https://rxl3xw6fxlkw.usemoralis.com:2053/server";
+const appId = "vZFNiJrNY3xmzpPW065LWJ0Rdeoy40dplLhMAvfx";
+Moralis.start({ serverUrl, appId });
+
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+   } catch(error) {
+     console.log(error)
+   }
+  }
+}
+
+login();
+
+
+
+/** Useful Resources  */
+
+// https://docs.moralis.io/moralis-server/users/crypto-login
+// https://docs.moralis.io/moralis-server/getting-started/quick-start#user
+// https://docs.moralis.io/moralis-server/users/crypto-login#metamask
+
+/** Moralis Forum */
+
+// https://forum.moralis.io/
